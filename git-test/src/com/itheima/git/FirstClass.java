ba29a40fda8cbae4b1be17eefdda1e868dcead67
danny <danny@163.com>
2020-11-23 11:53:53 +0800

    添加test函数

--- git-test/src/com/itheima/git/FirstClass.java
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -8,6 +8,8 @@ public class FirstClass {
         System.out.println("hello git");
     }
     public void test() {
+        int i=0;
+        i += 10;
         System.out.println("test");
     }
 }
